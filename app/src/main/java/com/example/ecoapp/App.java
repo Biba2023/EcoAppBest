fa5a68b66aab75de9fc7d9ac1663f75a3234ff69
НР-РС <drnaleks@gmail.com>
2024-04-25 08:19:54 +0300

    U can see Login Screen only 1 time

---
+++ app/src/main/java/com/example/ecoapp/App.java
@@ -0,0 +1,12 @@
+package com.example.ecoapp;
+
+import android.app.Application;
+
+public class App extends Application {
+    public static String points = "Баллы";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
--- app/src/main/java/com/example/ecoapp/Login.java
+++ app/src/main/java/com/example/ecoapp/Login.java
@@ -1,10 +1,7 @@
 package com.example.ecoapp;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
