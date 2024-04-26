ffc13e585fdd629b0764949e3d852a07bb6a9bdc
MrRent.ru <drnaleks@gmail.com>
2024-04-26 04:36:34 +0300

    Added name in profile screen

--- app/src/main/java/com/example/ecoapp/ui/profile/NotificationsFragment.java
+++ app/src/main/java/com/example/ecoapp/ui/profile/NotificationsFragment.java
@@ -1,5 +1,9 @@
 package com.example.ecoapp.ui.profile;
 
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,7 +19,8 @@ import com.example.ecoapp.databinding.FragmentNotificationsBinding;
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
-
+    private static final String PREF_NAME = "UserData";
+    private TextView Name;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         NotificationsViewModel notificationsViewModel =
@@ -23,7 +28,12 @@ public class NotificationsFragment extends Fragment {
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        Name = binding.name;
+        SharedPreferences sharedPreferences = requireActivity().getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
 
+        String login = sharedPreferences.getString("login", "");
+
+        Name.setText(login);
 
         return root;
     }
